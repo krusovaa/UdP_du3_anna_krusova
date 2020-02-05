f8c5a9536ae43ded445fb1a2ff8819eab9ee44a2
Anna Krusova <anicka.krusova@gmail.com>
2020-02-05 14:28:08 +0100

    pridan for cyklus ve funkci split_gjs_by_geometry a drobne upravy

--- du3_anna_krusova.py
+++ du3_anna_krusova.py
@@ -1,47 +1,54 @@
-import glob, json, os, sys
+import json, os, sys
+from pathlib import Path
 
 
 def add_filepath(file):
-    """add absolute path of each file to files's properties and print print the directory being browsed"""
+    """add absolute path of each file to files's properties"""
     file['features']['properties']['filepath'] = os.path.abspath(file)
 
-    print(os.path.dirname(os.path.abspath(file)))
 
-
-def split_gjs_by_geometry(direct):
+def split_gjs_by_geometry():
     """open geojson and json files and merge all point/linestirng/polygon features into separate geojsons"""
+
     points_geojson = []
     lines_geojson = []
     polygons_geojson = []
 
-    for f in glob.glob([direct + '/**/*.geojson', direct + '/**/*.json'], recursive=True):
-        with open(f, 'r+', encoding='utf-8') as infile:
-            data = json.load(infile)
+    extensions = ['*.geojson', '*.json']
+
+    for e in extensions:
+        for filepath in Path(sys.argv[1]).rglob(e):
             try:
-                if data['features']['geometry']['type'] == 'Point':
-                    add_filepath(data)
-                    points_geojson.append(data)
-                elif data['features']['geometry']['type'] == 'Linestring':
-                    add_filepath(data)
-                    lines_geojson.append(data)
-                elif data['features']['geometry']['type'] == 'Polygon':
-                    add_filepath(data)
-                    polygons_geojson.append(data)
-                else:
-                    print('Invalid GeoJSON'.format(data))
-            except ValueError as e:
-                print('Invalid GeoJSON:'.format(data), e)
+                # noinspection PyTypeChecker
+                with open(filepath, 'r+', encoding='utf-8') as f:
+                    try:
+                        print(filepath)
+                        data = json.load(f)
+                        for feat in data['features']:
+                            if feat['geometry']['type'] == 'Point':
+                                feat['filepath'] = str(filepath)
+                                points_geojson.append(feat)
+                            elif feat['geometry']['type'] == 'LineString':
+                                feat['filepath'] = str(filepath)
+                                lines_geojson.append(feat)
+                            elif feat['geometry']['type'] == 'Polygon':
+                                feat['filepath'] = str(filepath)
+                                polygons_geojson.append(feat)
+                            else:
+                                pass
+                    except json.JSONDecodeError:
+                        print('Invalid JSON format: ', filepath)
+            except PermissionError:
+                print('Invalid JSON format: ', filepath)
 
-    with open('points.geojson', 'w', encoding='utf-8') as outfile:
-        json.dump(points_geojson, outfile, indent=2, ensure_ascii=False)
-    with open('lines.geojson', 'w', encoding='utf-8') as outfile:
-        json.dump(lines_geojson, outfile, indent=2, ensure_ascii=False)
-    with open('polygons.geojson', 'w', encoding='utf-8') as outfile:
-        json.dump(polygons_geojson, outfile, indent=2, ensure_ascii=False)
+    with open("points.geojson", "w", encoding="utf-8") as outfile1:
+        json.dump(points_geojson, outfile1, indent=2, ensure_ascii=False)
+
+    with open("lines.geojson", "w", encoding="utf-8") as outfile2:
+        json.dump(lines_geojson, outfile2, indent=2, ensure_ascii=False)
+
+    with open("polygons.geojson", "w", encoding="utf-8") as outfile3:
+        json.dump(polygons_geojson, outfile3, indent=2, ensure_ascii=False)
 
 
-# directory as a command line argument, set to working directory
-directory = sys.argv[1]
-os.chdir(os.path.abspath(directory))
-
-split_gjs_by_geometry(directory)
+split_gjs_by_geometry()
