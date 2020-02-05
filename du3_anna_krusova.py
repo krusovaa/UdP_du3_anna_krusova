0ba83356c2defa5961ec14b76d4c1e925558a433
Anna Krusova <anicka.krusova@gmail.com>
2020-02-05 14:49:05 +0100

    doplneny komentare, pouziti add_filepath v hlavni fuknci + drobne upravy

--- du3_anna_krusova.py
+++ du3_anna_krusova.py
@@ -1,45 +1,54 @@
-import json, os, sys
+# import modules
+import json
+import os
+import sys
 from pathlib import Path
 
 
 def add_filepath(file):
     """add absolute path of each file to files's properties"""
-    file['features']['properties']['filepath'] = os.path.abspath(file)
+    file['filepath'] = os.path.abspath(str(file))
 
 
 def split_gjs_by_geometry():
     """open geojson and json files and merge all point/linestirng/polygon features into separate geojsons"""
 
+    # empty output lists
     points_geojson = []
     lines_geojson = []
     polygons_geojson = []
 
+    # select extensions being browsed
     extensions = ['*.geojson', '*.json']
 
     for e in extensions:
+        # directory gained from command line
         for filepath in Path(sys.argv[1]).rglob(e):
             try:
                 # noinspection PyTypeChecker
                 with open(filepath, 'r+', encoding='utf-8') as f:
                     try:
+                        # print file being browsed
                         print(filepath)
                         data = json.load(f)
                         for feat in data['features']:
+                            # select file by geometry
                             if feat['geometry']['type'] == 'Point':
-                                feat['filepath'] = str(filepath)
+                                add_filepath(feat)
                                 points_geojson.append(feat)
                             elif feat['geometry']['type'] == 'LineString':
-                                feat['filepath'] = str(filepath)
+                                add_filepath(feat)
                                 lines_geojson.append(feat)
                             elif feat['geometry']['type'] == 'Polygon':
-                                feat['filepath'] = str(filepath)
+                                add_filepath(feat)
                                 polygons_geojson.append(feat)
                             else:
-                                pass
+                                print('Unable to identify geometry type: ', filepath)
+                                continue
                     except json.JSONDecodeError:
                         print('Invalid JSON format: ', filepath)
             except PermissionError:
-                print('Invalid JSON format: ', filepath)
+                print('Not adequate access rights: ', filepath)
 
     with open("points.geojson", "w", encoding="utf-8") as outfile1:
         json.dump(points_geojson, outfile1, indent=2, ensure_ascii=False)
